57d83cb562551e9b2651adcc396d3c4fdd29b1d8
DispicableLee <lee.robert053@gmail.com>
2022-11-01 13:08:37 -0400

    done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def line(array)
+    puts "the line is currently emptey" if array.length == 0
+    x = 1
+    z = 0
+    while x <= array.length && z < array.length
+        puts "#{array[z]}, you are ##{x}"
+        x += 1
+        z += 1
+    end
+end
+
+line(["Ariel", "RObert"])
+
+
+def take_a_number(array, name)
+    array.push(name)
+    puts "#{name}, you are ##{array.length}"
+end
+
+take_a_number(["Ariel", "RObert"], "John")
+
+def now_serving(array)
+    puts "There is nobody waiting to be served!" if array.length == 0
+    puts "#{array[0]}, You're next!"
+    array.shift()
+    puts array
+end
+
+now_serving(["Ariel", "Rob"])
